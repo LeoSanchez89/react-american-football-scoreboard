c506ca2a5c067c2eec58a46a5d1ae35aa0ab9c13
LeoSanchez89 <58442719+LeoSanchez89@users.noreply.github.com>
2020-01-20 17:58:29 -0500

    added reset button and changed button alignment

--- src/App.js
+++ src/App.js
@@ -4,61 +4,67 @@ import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
-  //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
-  const [homeScore, setHome] = useState(32);
-  const [awayScore, setAway] = useState(32);
-  return (
-    <div className="container">
-      <section className="scoreboard">
-        <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
+	//TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+	const [homeScore, setHome] = useState(32);
+	const [awayScore, setAway] = useState(32);
+	return (
+		<div className="container">
+			<section className="scoreboard">
+				<div className="topRow">
+					<div className="home">
+						<h2 className="home__name">Lions</h2>
 
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
+						{/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">{homeScore}</div>
-          </div>
-          <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awayScore}</div>
-          </div>
-        </div>
-        <BottomRow />
-      </section>
-      <section className="buttons">
-        <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button
-            className="homeButtons__touchdown"
-            onClick={event => setHome(homeScore + 7)}
-          >
-            Home Touchdown
+						<div className="home__score">{homeScore}</div>
+					</div>
+					<div className="timer">00:03</div>
+					<div className="away">
+						<h2 className="away__name">Tigers</h2>
+						<div className="away__score">{awayScore}</div>
+					</div>
+				</div>
+				<BottomRow />
+			</section>
+			<section className="buttons">
+				<div className="homeButtons">
+					{/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
+					<button
+						className="homeButtons__touchdown"
+						onClick={event => setHome(homeScore + 7)}
+					>
+						Home Touchdown
 					</button>
-          <button
-            className="homeButtons__fieldGoal"
-            onClick={event => setHome(homeScore + 3)}
-          >
-            Home Field Goal
+					<button
+						className="homeButtons__fieldGoal"
+						onClick={event => setHome(homeScore + 3)}
+					>
+						Home Field Goal
 					</button>
-        </div>
-        <div className="awayButtons">
-          <button
-            className="awayButtons__touchdown"
-            onClick={event => setAway(awayScore + 7)}
-          >
-            Away Touchdown
+					<button className="homeButtons__reset" onClick={event => setHome(0)}>
+						Score Reset
 					</button>
-          <button
-            className="awayButtons__fieldGoal"
-            onClick={event => setAway(awayScore + 3)}
-          >
-            Away Field Goal
+				</div>
+				<div className="awayButtons">
+					<button
+						className="awayButtons__touchdown"
+						onClick={event => setAway(awayScore + 7)}
+					>
+						Away Touchdown
 					</button>
-        </div>
-      </section>
-    </div>
-  );
+					<button
+						className="awayButtons__fieldGoal"
+						onClick={event => setAway(awayScore + 3)}
+					>
+						Away Field Goal
+					</button>
+					<button className="awayButtons__reset" onClick={event => setAway(0)}>
+						Score Reset
+					</button>
+				</div>
+			</section>
+		</div>
+	);
 }
 
 export default App;
